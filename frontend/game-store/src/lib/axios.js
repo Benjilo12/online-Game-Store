c17ba329b8871100ad53f842347245d649ed06d4
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2025-08-20 17:31:45 +0000

    Update axios.js

--- frontend/game-store/src/lib/axios.js
+++ frontend/game-store/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+    import.meta.mode === "development" ? "https://rockz-gamez-backend.onrender.com" : "/api",
   withCredentials: true, // send cookies to the server
 });
 
